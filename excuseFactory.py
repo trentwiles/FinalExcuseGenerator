2d62df6413d7c995790e5e2208b1067e4017e554
trentwiles <wilest7@gmail.com>
2023-09-13 14:39:51 -0700

    work for today

--- excuseFactory.py
+++ excuseFactory.py
@@ -14,8 +14,9 @@ def chatGPT(message):
     return completion.choices[0].message["content"]
 
 def generateExcuse(pro, location, name):
+    print(pro + "" + location + name)
     if location == "":
         return chatGPT(f"Create an excuse about why you can't attend your job as a tennis pro. You must address it in \"text message\" format to your boss, {pro}, and make sure to specficy the event happens at a random location you, the AI, create. The excuse doesn't have to involve tennis, but it can. My name is {name}.")
     return chatGPT(f"Create an excuse about why you can't attend your job as a tennis pro. You must address it in \"text message\" format to your boss, {pro}, and this event must take place in {location}. Keep it short, but include details. The excuse doesn't have to involve tennis, but it can. My name is {name}.")
 
-print(generateExcuse(pro="Chris", location="", name="Trent"))
+#print(generateExcuse(pro="Chris", location="", name="Trent"))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import excuseFactory
+import sys
 
 app = Flask(__name__)
 
@@ -9,7 +10,13 @@ def index():
 
 @app.route('/', methods=["POST"])
 def indexWithPost():
-    return render_template("index.html", message=excuseFactory.generateExcuse(name=request.args.get("name"), location=request.args.get("l"), pro=request.args.get("pro")))
+    print(request.args)
+    location = request.args.get("l")
+    if request.args.get("l") == None:
+        location = ""
+    pro = request.args.get("p")
+    name = request.args.get("name")
+    return render_template("index.html", message=excuseFactory.generateExcuse(pro, location, name))
 
 if __name__ == '__main__':
     app.run()
