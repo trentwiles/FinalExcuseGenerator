64730218d12a6d5bf3fcda8c4a4045e5b1c34554
trentwiles <wilest7@gmail.com>
2023-09-13 14:23:09 -0700

    big shit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import excuseFactory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/', methods=["POST"])
+def indexWithPost():
+    return render_template("index.html", message=excuseFactory.generateExcuse(name=request.args.get("name"), location=request.args.get("l"), pro=request.args.get("pro")))
 
 if __name__ == '__main__':
     app.run()
